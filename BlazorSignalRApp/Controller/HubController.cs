89c5f00ed1c5c838f8bc0084a58cc332ef440456
mkm1999 <mohammadkarbalaee98@gmail.com>
2024-12-20 21:33:59 +0330

    Add project files.

---
+++ BlazorSignalRApp/Controller/HubController.cs
@@ -0,0 +1,32 @@
+﻿using BlazorSignalRApp.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorSignalRApp.Controller;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HubController(IHubContext<ChatHub> _hub) : ControllerBase
+{
+	[HttpGet]
+	public async Task<bool> Get(string Message,string GroupName)
+	{
+		await _hub.Clients.Group(GroupName).SendAsync("ReceiveMessage", new
+		{
+			Message
+		},
+		new
+		{
+			Message,
+		}
+		);
+		return true;
+	}
+
+	[HttpGet("Join")]
+	public async Task<string> Join(string ConnectionId,string GroupName)
+	{
+		await _hub.Groups.AddToGroupAsync(ConnectionId, GroupName);
+		return $"{GroupName} - {ConnectionId}";
+	}
+}
---
+++ BlazorSignalRApp/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorSignalRApp.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.Group("await Groups.AddToGroupAsync(Context.ConnectionId, \"1\");").SendAsync("ReceiveMessage", user, message);
+    }
+
+    public string GetConnectionId() => Context.ConnectionId;
+}
